dc0b08d2017882ce4b3419af51bd93b591e534b9
acichecki <a.cichecki@gmail.com>
2019-01-09 09:54:28 +0100

    DirectoryItemModel

---
+++ SanityArchiver/SanityArchiver/Model/DirectoryItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanityArchiver.Model
+{
+    class DirectoryItemModel
+    {
+        private ObservableCollection<DriveInfo> drivesInfos = new ObservableCollection<DriveInfo>(Environment.GetLogicalDrives().Select(drive => new DriveInfo(drive) ));
+
+        public ObservableCollection<DriveInfo> DrivesInfos
+        {
+            get { return drivesInfos; }
+        }
+
+        public string Gowno
+        {
+            get { return "gowno"; }
+        }
+    }
+}
